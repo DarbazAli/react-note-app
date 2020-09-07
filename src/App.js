8ac3f027bff40bcff3c9174d4b86dea11be392d8
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-07 13:12:38 +0300

    move notelist to parent component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { render } from "react-dom";
+
+
 import Note from "./components/Note";
 import NewNoteButton from "./components/NewNoteButton";
 import NoteList from "./components/NoteList";
+
+
+// import NoteItem from './NoteItem'
+
+const noteList = [
+    {
+      title: "Sample note #1",
+      desc: "Note description #1",
+      date: new Date().toDateString(),
+    },
+
+    {
+        title: "Sample note #2",
+        desc: "Note description #2",
+        date: new Date().toDateString(),
+      },
+];
+
+const store = window.localStorage;
+store.setItem("note_list", JSON.stringify(noteList));
+const list = JSON.parse(store.getItem("note_list"));
 
 const App = () => {
   return (
     <div>
       <NewNoteButton />
       {/* <Note /> */}
-      <NoteList />
+      <NoteList notes={list}/>
     </div>
   );
 };
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,31 +1,16 @@
 import React from 'react'
-import NoteItem from './NoteItem'
+import NoteItem from "./NoteItem";
 
-const noteList = [
-    {
-      title: "Sample note #1",
-      desc: "Note description #1",
-      date: new Date().toDateString(),
-    },
-
-    {
-        title: "Sample note #2",
-        desc: "Note description #2",
-        date: new Date().toDateString(),
-      },
-];
-
-const store = window.localStorage;
-store.setItem("note_list", JSON.stringify(noteList));
-
-
-const NoteList = () => {
-    const list = JSON.parse(store.getItem("note_list"));
+const NoteList = ({notes}) => {
     return (
         <div>
           {
-              list.map( item => {
-                  return <NoteItem key={item.title} title={item.title} date={item.date} />
+              notes.map( item => {
+                  return <NoteItem 
+                            key={item.title} 
+                            title={item.title} 
+                            date={item.date} 
+                        />
               })
           }
         </div>
@@ -33,13 +18,3 @@ const NoteList = () => {
 }
 
 export default NoteList
-
-
-
-
-
-
-
-
-
-// console.log(list[0].title);
